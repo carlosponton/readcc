17438d2142c6e1655932c6488cf81ec25048c330
Carlos Pontón <carlos.ponton@coraltech.co>
2020-04-24 13:43:57 -0500

    build app

---
+++ precache-manifest.f4914d83c0480e16a6005dbaf003cea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7ce30655d8fda488554ddd8d31f635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "323cb931d2310217a817",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "55da6f59300a6544aacc",
+    "url": "/static/js/2.0b8da377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b8da377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323cb931d2310217a817",
+    "url": "/static/js/main.dffb7901.chunk.js"
+  },
+  {
+    "revision": "7a1219d5267809ef0909",
+    "url": "/static/js/runtime-main.4248c55a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
